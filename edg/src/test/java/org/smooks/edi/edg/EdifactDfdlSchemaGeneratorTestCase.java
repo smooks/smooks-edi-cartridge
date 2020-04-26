063c5597b59f735ec6c9134115952f8249bba234
claude.mamo <claude.mamo@gmail.com>
2020-04-26 12:49:44 +0200

    Fixed schema errors; generating maxOccurs for components; expanded test coverage; moved common schemas into their own module; upgraded dependencies

--- edg/src/test/java/org/smooks/edi/edg/EdifactDfdlSchemaGeneratorTestCase.java
+++ edg/src/test/java/org/smooks/edi/edg/EdifactDfdlSchemaGeneratorTestCase.java
@@ -1,22 +1,56 @@
 package org.smooks.edi.edg;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import org.apache.commons.io.FileUtils;
+import org.apache.daffodil.tdml.Runner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import scala.Option;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EdifactDfdlSchemaGeneratorTestCase {
 
+    @BeforeEach
+    public void beforeEach() throws IOException {
+        FileUtils.deleteQuietly(new File("target/generated-test-resources"));
+    }
+
     @Test
     public void testMain() throws Throwable {
-        assertFalse(new File("target/d03b/EDIFACT-Segments.dfdl.xsd").exists());
-        assertFalse(new File("target/d03b/EDIFACT-Messages.dfdl.xsd").exists());
+        assertFalse(new File("target/generated-test-resources/d03b/EDIFACT-Segments.dfdl.xsd").exists());
+        assertFalse(new File("target/generated-test-resources/d03b/EDIFACT-Messages.dfdl.xsd").exists());
 
-        EdifactDfdlSchemaGenerator.main(new String[]{"/d03b.zip", "target/"});
+        EdifactDfdlSchemaGenerator.main(new String[]{"/d03b.zip", "target/generated-test-resources"});
 
-        assertTrue(new File("target/d03b/EDIFACT-Segments.dfdl.xsd").exists());
-        assertTrue(new File("target/d03b/EDIFACT-Messages.dfdl.xsd").exists());
+        assertTrue(new File("target/generated-test-resources/d03b/EDIFACT-Segments.dfdl.xsd").exists());
+        assertTrue(new File("target/generated-test-resources/d03b/EDIFACT-Messages.dfdl.xsd").exists());
+    }
+
+    @Test
+    public void testDfdlSchema() throws Throwable {
+        EdifactDfdlSchemaGenerator.main(new String[]{"/d03b.zip", "target/generated-test-resources"});
+
+        final File generatedSchema = new File("target/generated-test-resources/d03b/EDIFACT-Message.dfdl.xsd");
+        Mustache mustache = new DefaultMustacheFactory().compile("EDIFACT-Common/EDIFACT-Message.dfdl.xsd.mustache");
+        try (FileWriter fileWriter = new FileWriter(generatedSchema)) {
+            mustache.execute(fileWriter, new HashMap<String, Object>() {{
+                this.put("schemaLocation", "EDIFACT-Messages.dfdl.xsd");
+                this.put("messages", Arrays.asList("INVOIC", "PAXLST"));
+                this.put("version", "D03B");
+            }});
+        }
+
+        Runner runner = new Runner(null, "", "parse.tdml", true, true, false, Runner.defaultRoundTripDefaultDefault(), Runner.defaultValidationDefaultDefault(), Runner.defaultImplementationsDefaultDefault());
+        runner.runOneTest("PAXLST", Option.empty(), true);
+        runner.runOneTest("INVOIC", Option.empty(), true);
     }
 }
