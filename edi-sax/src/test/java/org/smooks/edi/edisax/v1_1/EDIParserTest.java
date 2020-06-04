fd8f792c7a4e137615c966d2b9e55db8f2867e4a
claude.mamo <claude.mamo@gmail.com>
2020-06-04 10:06:14 +0200

    Removed old license

--- edi-sax/src/test/java/org/smooks/edi/edisax/v1_1/EDIParserTest.java
+++ edi-sax/src/test/java/org/smooks/edi/edisax/v1_1/EDIParserTest.java
@@ -40,22 +40,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * =========================LICENSE_END==================================
  */
-/*
-	Milyn - Copyright (C) 2006 - 2010
-
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License (version 2.1) as published by the Free Software 
-	Foundation.
-
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    
-	See the GNU Lesser General Public License for more details:    
-	http://www.gnu.org/licenses/lgpl.txt
-*/
-
 package org.smooks.edi.edisax.v1_1;
 
 import org.smooks.edi.edisax.AbstractEDIParserTestCase;
